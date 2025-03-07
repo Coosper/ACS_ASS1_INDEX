d5855dad7cf373ffdaf70fc099c3d4b5b510c367
Kacper Marszelewski <116608850+Coosper@users.noreply.github.com>
2025-03-07 23:37:56 +0000

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -7,9 +7,18 @@ INSTANCE_ID=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.2
 MEMORYUSAGE=$(free -m | awk 'NR==2{printf "%.2f%%", $3*100/$2 }')
 PROCESSES=$(expr $(ps -A | grep -c .) - 1)
 HTTPD_PROCESSES=$(ps -A | grep -c httpd)
+CPUUSAGE=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}' | awk '{printf "%.2f%%", $1}')
 
+echo "               /\_____/\ "
+echo "              /  o   o  \ "
+echo "             ( ==  ^  == ) "
+echo "              )         ( "
+echo "             (           ) "
+echo "            ( (  )   (  ) ) "
+echo "           (__(__)___(__)__)"
 echo "Instance ID: $INSTANCE_ID"
 echo "Memory utilisation: $MEMORYUSAGE"
+echo "CPU utilisation: $CPUUSAGE"
 echo "No of processes: $PROCESSES"
 if [ $HTTPD_PROCESSES -ge 1 ]
 then
